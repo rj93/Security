d15c114e416aafd617deeeb9bfb5ca0774e3b1fb
Richard Jones <contact@richardjones.io>
2016-10-18 22:53:18 +0100

    updated part a print format

--- parta.py
+++ parta.py
@@ -1,5 +1,8 @@
-import string, cipher, re
+# python parta.py ciphertext.txt
+
+import string, re
 from itertools import product
+from cipher import convert, decrypt
 
 KEY_LENGTH = 6
 CHARS = string.ascii_lowercase
@@ -10,7 +13,7 @@ def brute_force(ciphertext, key_length=KEY_LENGTH):
 	products = product(CHARS, repeat=key_length)
 	for chars in products:
 		key = ''.join(chars)
-		plaintext = cipher.decrypt(cipher.convert(key), ciphertext)
+		plaintext = decrypt(convert(key), ciphertext)
 		if regex.search(plaintext) is not None:
 			return key, plaintext
 
@@ -21,4 +24,6 @@ if __name__ == '__main__':
 	ciphertext = file.read()
 	file.close()
 
-	print(brute_force(ciphertext))
+	key, plaintext = brute_force(ciphertext)
+	print(key)
+	print(plaintext)
