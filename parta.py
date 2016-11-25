cd14ce13f233827ea96167de74288b4d3a8e61c1
Richard Jones <contact@richardjones.io>
2016-11-25 17:46:19 +0000

    implemented brute force to be multithreaded (part a)

--- parta.py
+++ parta.py
@@ -1,13 +1,15 @@
 # python parta.py ciphertext.txt
 
-import string, re, time
+import string, re, time, sys
 from itertools import product
 from cipher import streamcipher
+from multiprocessing import Pool, cpu_count
 
+PRCOESSES = max(cpu_count() - 1, 1) 
 KEY_LENGTH = 4
 CHARS = string.ascii_lowercase
 
-regex = re.compile('^[\w\s.,!?:;_\'\"\(\)\[\]\{\}\-]+$') # matches a-z, A-Z, 0-9, and .,!?:;'"()[]{}-
+regex = re.compile('^[\w\s.,!?:;_\'\"\(\)\[\]\{\}\-]+$') # matches a-z, A-Z, 0-9, any whitespace character, and .,!?:;'"()[]{}-
 
 def read_file(file):
 	""" returns the contents of a file """
@@ -23,33 +25,48 @@ def decrypt(key, ciphertext):
 	keystream = streamcipher(key)
 	return ''.join([chr(int(ciphertext[n:n+2], 16) ^ next(keystream)) for n in range(len(ciphertext)) if n % 2 == 0]) 
 
-def brute_force(ciphertext, key_length=KEY_LENGTH, count=False):
-	""" brute forces the key of the ciphertext.
-		Returns the key, the plaintext, 
-		and dependant on the count flag how many attempts it took. """
-		
-	products = product(CHARS, repeat=key_length)
-	attempts = 0
-	for chars in products:
-		attempts = attempts + 1
-		key = ''.join(chars)
-		plaintext = decrypt(convert(key), ciphertext)
-		if regex.search(plaintext) is not None:
-			if not count:
-				return key, plaintext
-			else:
-				return key, plaintext, attempts
+def attempt_decrypt(chars):
+	""" performs a decrypt, and checks to 
+	see if the plaintext is accepted by the regex. 
+	if accepted returns dictionary containing 
+	key and plaintext, otherwise returns None """
 
-if __name__ == '__main__':
-	import sys
-	
-	ciphertext = read_file(sys.argv[1])
+	key = ''.join(chars)
+	plaintext = decrypt(convert(key), ciphertext)
+	if regex.search(plaintext) is not None:
+		return {'key' : key, 'plaintext' : plaintext}
+
+def brute_force(key_length=KEY_LENGTH, processes=PRCOESSES):
+	""" brute forces the key of the ciphertext.
+		Returns a dictionary with the key, plaintext,
+		key_attempts, total_time, and keys_per_second """
+
+	products = product(CHARS, repeat=key_length)
+	pool = Pool(processes=processes)
+	key_attempts = 0
 
 	start = time.time()
-	key, plaintext, keys = brute_force(ciphertext, count=True)
-	totalTime = round(time.time() - start, 2)
-	keysPerSec = round(keys / totalTime)
+	for result in pool.imap(attempt_decrypt, products): 
+		key_attempts = key_attempts + 1
+		if result:
+			break
+	total_time = round(time.time() - start, 2)
+	pool.close()
 
-	print(key)
-	print(plaintext)
-	print('time = {}s, keys = {}, keys/second = {}'.format(totalTime, keys, keysPerSec))
+	if result:
+		keys_per_sec = round(key_attempts / total_time)
+		result['key_attempts'] = key_attempts
+		result['total_time'] = total_time
+		result['keys_per_sec'] = keys_per_sec
+		return result
+
+if __name__ == '__main__':
+	
+	ciphertext = read_file(sys.argv[1])
+	result = brute_force()
+	if result:
+		print(result['key'])
+		print(result['plaintext'])
+		print('time = {}s, key attempts = {}, keys/second = {}'.format(result['total_time'], result['key_attempts'], result['keys_per_sec']))
+	else:
+		print("Unable to find key")
